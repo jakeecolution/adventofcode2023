a225804010258e45af41a517c31bf3b8b45a4f51
Jacob Bryan <jake@ecojake.dev>
2023-12-27 21:29:28 -0500

    Completed day 3 part a & b

--- day3/main.go
+++ day3/main.go
@@ -12,10 +12,16 @@ const (
 	fnum   rune = '0'
 	lnum   rune = '9'
 	period rune = '.'
+	star   rune = '*'
 )
 
 type Slice struct {
 	first, last int
+}
+
+type SliceContext struct {
+	slice Slice
+	line  int
 }
 
 func getNumbers(line []rune) []Slice {
@@ -42,8 +48,39 @@ func getNumbers(line []rune) []Slice {
 			}
 		}
 	}
+	if cur.first > -1 && cur.last > -1 {
+		tmp := Slice{first: cur.first, last: cur.last}
+		slices = append(slices, tmp)
+	} else if cur.first > -1 {
+		tmp := Slice{first: cur.first, last: cur.first}
+		slices = append(slices, tmp)
+	}
 	return slices
 
+}
+
+func getNumber(line []rune, pos int) Slice {
+	var lrange, rrange int
+	for i := pos; i >= 0; i-- {
+		if line[i] <= lnum && line[i] >= fnum {
+			lrange = i
+		} else {
+			break
+		}
+	}
+	for i := pos; i < len(line); i++ {
+		if line[i] <= lnum && line[i] >= fnum {
+			rrange = i
+		} else {
+			break
+		}
+	}
+	return Slice{first: lrange, last: rrange}
+}
+
+func getNumberInt(line []rune, pos int) int {
+	sliced := getNumber(line, pos)
+	return stringToInt(string(line[sliced.first : sliced.last+1]))
 }
 
 func stringToInt(slice string) int {
@@ -67,6 +104,7 @@ func isPartNumber(num Slice, curline []rune, preline []rune, nexline []rune) int
 	} else {
 		rrange = num.last
 	}
+
 	if preline != nil {
 		for i := lrange; i <= rrange; i++ {
 			if (preline[i] > lnum || preline[i] < fnum) && (preline[i] != period) {
@@ -90,7 +128,7 @@ func isPartNumber(num Slice, curline []rune, preline []rune, nexline []rune) int
 		}
 	}
 	if lrange != num.first && rrange != num.last {
-		if ((curline[lrange] > lnum || curline[num.first-lrange] < fnum) && (curline[lrange] != period)) || ((curline[rrange] > lnum || curline[rrange] < fnum) && (curline[rrange] != period)) {
+		if (curline[lrange] != period) || (curline[rrange] != period) {
 			if num.first == num.last {
 				return stringToInt(string(curline[num.first]))
 			}
@@ -98,14 +136,14 @@ func isPartNumber(num Slice, curline []rune, preline []rune, nexline []rune) int
 		}
 
 	} else if num.first == lrange && num.last != rrange {
-		if (curline[rrange] > lnum || curline[rrange] < fnum) && (curline[rrange] != period) {
+		if curline[rrange] != period {
 			if num.first == num.last {
 				return stringToInt(string(curline[num.first]))
 			}
 			return stringToInt(string(curline[num.first : num.last+1]))
 		}
 	} else if num.last == rrange && num.first != lrange {
-		if (curline[lrange] > lnum || curline[lrange] < fnum) && (curline[lrange] != period) {
+		if curline[lrange] != period {
 			if num.first == num.last {
 				return stringToInt(string(curline[num.first]))
 			}
@@ -118,17 +156,10 @@ func isPartNumber(num Slice, curline []rune, preline []rune, nexline []rune) int
 func Exercise1(input [][]rune) {
 	fmt.Println("Exercise 1")
 	var sum, cur int
-	reader := bufio.NewReader(os.Stdin)
 	for idx, line := range input {
-		// fmt.Printf("Line %d:", idx)
 		slices := getNumbers(line)
-		fmt.Printf("Line %d: ", idx)
 		for _, slice := range slices {
-			// if slice.last != -1 {
-			// 	fmt.Printf("%s, ", string(line[slice.first:slice.last+1]))
-			// } else {
-			// 	fmt.Printf("%s, ", string(line[slice.first]))
-			// }
+
 			if idx == 0 && len(input) > idx+1 {
 				cur = isPartNumber(slice, line, nil, input[idx+1])
 			} else if idx == len(input)-1 {
@@ -136,28 +167,121 @@ func Exercise1(input [][]rune) {
 			} else {
 				cur = isPartNumber(slice, line, input[idx-1], input[idx+1])
 			}
-			fmt.Printf("%d, ", cur)
-			sum += cur
+			if cur != 0 {
+				sum += cur
+			}
 		}
 
-		if idx > 0 {
-			fmt.Printf("\n%s", string(input[idx-1]))
-		}
-		fmt.Printf("\n%s", string(line))
-		if idx < len(input)-1 {
-			fmt.Printf("\n%s", string(input[idx+1]))
-		}
-		fmt.Println("\nCheck line above: ")
-		_, _ = reader.ReadString('\n')
 	}
 	fmt.Println("Sum: ", sum)
+}
+
+func IsDigit(char rune) bool {
+	return char >= fnum && char <= lnum
+}
+
+func isGearStar(spos int, curline []rune, preline []rune, nexline []rune) int {
+	var lrange, rrange int
+	if spos >= 1 {
+		lrange = spos - 1
+	} else {
+		lrange = 0
+	}
+	if spos < len(curline)-1 {
+		rrange = spos + 1
+	} else {
+		rrange = spos
+	}
+	nums := make([]int, 0)
+	if preline != nil {
+		for i := lrange; i <= rrange; i++ {
+			if IsDigit(preline[i]) {
+				sliced := getNumber(preline, i)
+				nums = append(nums, stringToInt(string(preline[sliced.first:sliced.last+1])))
+				if sliced.first <= spos && sliced.last >= spos {
+					break
+				}
+			}
+		}
+	}
+	if nexline != nil {
+		for i := lrange; i <= rrange; i++ {
+			if IsDigit(nexline[i]) {
+				sliced := getNumber(nexline, i)
+				nums = append(nums, stringToInt(string(nexline[sliced.first:sliced.last+1])))
+				if sliced.first <= spos && sliced.last >= spos {
+					break
+				}
+			}
+		}
+	}
+	if lrange != spos && rrange != spos {
+		if IsDigit(curline[lrange]) {
+			nums = append(nums, getNumberInt(curline, lrange))
+		}
+		if IsDigit(curline[rrange]) {
+			nums = append(nums, getNumberInt(curline, rrange))
+		}
+
+	} else if spos == lrange && spos != rrange {
+		if IsDigit(curline[rrange]) {
+			nums = append(nums, getNumberInt(curline, rrange))
+		}
+	} else if spos == rrange && spos != lrange {
+		if IsDigit(curline[lrange]) {
+			nums = append(nums, getNumberInt(curline, lrange))
+		}
+	}
+	if len(nums) == 2 {
+		// fmt.Println("Gear: ", nums, " = ", nums[0]*nums[1])
+		return nums[0] * nums[1]
+	}
+	return 0
+}
+
+func Exercise2(input [][]rune) {
+	fmt.Println("Exercise 2")
+	var sum, mygear int
+	for idx, line := range input {
+		for index, myrune := range line {
+			if myrune != star {
+				continue
+			}
+			var lrange, rrange int
+			if index >= 1 {
+				lrange = index - 1
+			} else {
+				lrange = 0
+			}
+			if index < len(line)-1 {
+				rrange = index + 1
+			} else {
+				rrange = index
+			}
+			if idx == 0 && len(input) > idx+1 {
+				fmt.Printf("Selected at index %d: \n\t[%d:%d]=%s\n\t%s\n", index, lrange, rrange+1, string(line[lrange:rrange+1]), string(input[idx+1][lrange:rrange+1]))
+				// mygear = isGearStar(index, line, nil, input[idx+1])
+			} else if idx == len(input)-1 {
+				mygear = isGearStar(index, line, input[idx-1], nil)
+				// fmt.Printf("Selected at index %d: \n\t%s\n\t%s\n", index, string(input[idx-1][lrange:rrange+1]), string(line[lrange:rrange+1]))
+			} else {
+				mygear = isGearStar(index, line, input[idx-1], input[idx+1])
+				// fmt.Printf("Selected at index %d: \n\t%s\n\t[%d:%d]=%s\n\t%s\n", index, string(input[idx-1][lrange:rrange+1]), lrange, rrange+1, string(line[lrange:rrange+1]), string(input[idx+1][lrange:rrange+1]))
+			}
+			if mygear > 0 {
+				sum += mygear
+			}
+		}
+	}
+	fmt.Println("Gear Sum: ", sum)
+
 }
 
 func main() {
 	var inputfile string
 	var exercise1 bool
 	flag.StringVar(&inputfile, "input", "input.txt", "Input file")
-	flag.BoolVar(&exercise1, "exercise1", true, "Exercise 1 is True, Exercise 2 is False")
+	flag.BoolVar(&exercise1, "exercise1", false, "Exercise 1 is True, Exercise 2 is False")
 	flag.Parse()
 
 	file, err := os.Open(inputfile)
@@ -172,7 +296,8 @@ func main() {
 	height, width := 0, 0
 	for scanner.Scan() {
 		height++
-		if charlong := scanner.Text(); width != 0 && width < len(charlong) {
+		charlong := scanner.Text()
+		if width != 0 && width < len(charlong) {
 			width = len(charlong)
 		} else if width == 0 {
 			width = len(charlong)
@@ -197,6 +322,6 @@ func main() {
 	if exercise1 {
 		Exercise1(myrune)
 	} else {
-		fmt.Println("Exercise 2")
+		Exercise2(myrune)
 	}
 }
